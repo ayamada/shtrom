0954893eabe9de87a974e5ff3587fa4fff8c03fe
Takashi AOKI <federkasten@me.com>
2013-10-01 17:39:38 +0900

    add private repository setting

--- project.clj
+++ project.clj
@@ -3,6 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :repositories [["snapshots" {:url "https://nexus.xcoo.jp/content/repositories/snapshots"}]
+                 ["releases" {:url "https://nexus.xcoo.jp/content/repositories/releases"}]]
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.5"]]
   :plugins [[lein-ring "0.8.5"]]
