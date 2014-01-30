d466ca68bfd798668c2060e9bfaf0a98425ad6ff
Takashi AOKI <federkasten@me.com>
2014-01-29 23:03:56 -0800

    remove local repository settings from project.clj

--- project.clj
+++ project.clj
@@ -1,10 +1,8 @@
 (defproject shtrom "0.1.0-SNAPSHOT"
-  :description "histogram database"
-  :url "http://example.com/FIXME"
+  :description "Fastest histogram database"
+  :url "http://github.com/chrovis/shtrom"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :repositories [["snapshots" {:url "https://nexus.xcoo.jp/content/repositories/snapshots"}]
-                 ["releases" {:url "https://nexus.xcoo.jp/content/repositories/releases"}]]
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.logging "0.2.6"]
                  [javax.servlet/servlet-api "2.5"]
