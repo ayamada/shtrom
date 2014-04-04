953e17217e5b5afed5abd7893c4f8d637f385e4b
Takashi AOKI <federkasten@me.com>
2014-04-04 15:21:18 +0900

    bump up versions of dependencies

--- project.clj
+++ project.clj
@@ -3,14 +3,13 @@
   :url "http://github.com/chrovis/shtrom"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.logging "0.2.6"]
                  [ring/ring-core "1.2.2"]
                  [compojure "1.1.6"]
                  [ring/ring-jetty-adapter "1.2.2"]]
   :plugins [[lein-ring "0.8.2"]
-            [lein-midje "3.1.3"]
-            [lein-bin "0.3.4"]]
+            [lein-midje "3.1.3"]]
   :profiles {:dev {:dependencies [[ring-mock "0.1.5"]
                                   [midje "1.6.3"]
                                   [javax.servlet/servlet-api "2.5"]]}}
@@ -20,5 +19,4 @@
          :war-exclusions [#".+?\.config\.clj"]}
   :jar-exclusions [#".+?\.config\.clj"]
   :main shtrom.core.handler
-  :aot [shtrom.core.handler]
-  :bin {:name "shtrom"})
+  :aot [shtrom.core.handler])
