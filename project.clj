d64153701b5d59f785bdb09e139107b6ce7a516f
Takashi AOKI <federkasten@me.com>
2014-11-25 11:49:09 +0900

    fix project configurations

--- project.clj
+++ project.clj
@@ -4,10 +4,15 @@
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/tools.logging "0.3.0"]
-                 [ring/ring-core "1.3.0"]
-                 [compojure "1.1.8"]
-                 [ring/ring-jetty-adapter "1.3.0"]]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [org.slf4j/slf4j-log4j12 "1.7.7"]
+                 [log4j/log4j "1.2.17" :exclusions [javax.mail/mail
+                                                    javax.jms/jms
+                                                    com.sun.jmdk/jmxtools
+                                                    com.sun.jmx/jmxri]]
+                 [ring/ring-core "1.3.1"]
+                 [compojure "1.2.1"]
+                 [ring/ring-jetty-adapter "1.3.1"]]
   :plugins [[lein-ring "0.8.2"]
             [lein-midje "3.1.3"]]
   :profiles {:dev {:dependencies [[ring-mock "0.1.5"]
@@ -17,6 +22,7 @@
          :init shtrom.handler/init
          :port 3001
          :war-exclusions [#".+?\.config\.clj"]}
-  :jar-exclusions [#".+?\.config\.clj"]
+  :jar-exclusions [#".+?\.config\.clj"
+                   #"log4j\.properties"]
   :main shtrom.handler
-  :aot [shtrom.handler])
+  :aot :all)
