1df17b4e3b60d523c7112f7b53e561b2407299aa
atsuo yamada <yamada-git@tir.jp>
2016-02-08 15:17:35 +0900

    Remove a test by "test-dir"

--- test/shtrom/t_config.clj
+++ test/shtrom/t_config.clj
@@ -6,6 +6,5 @@
   (try
     (config/load-config)
     (catch Throwable e nil)) => anything
-  (config/load-config "test-dir") => (throws Throwable)
   (config/load-config "not-exist-file") => (throws Throwable))
 
