ce89d20ec7d49d080ee8a76a484027873758d40f
atsuo yamada <yamada-git@tir.jp>
2016-02-08 15:25:00 +0900

    Rename a temporary hist file name from utiltest.hist to dummy.hist

--- test/shtrom/t_util.clj
+++ test/shtrom/t_util.clj
@@ -7,7 +7,7 @@
 
 (defn- prepare-histfile! []
   (.mkdirs (io/file t-data/test-dir))
-  (let [histfile (str t-data/test-dir "/utiltest.hist")]
+  (let [histfile (str t-data/test-dir "/dummy.hist")]
     (spit histfile
           (apply str
                  (interpose "\n"
@@ -16,18 +16,18 @@
                                   {:val 222}
                                   {:val 33}
                                   {:val 44}])))))
-  (let [not-histfile (str t-data/test-dir "/utiltest.txt")]
+  (let [not-histfile (str t-data/test-dir "/dummy.txt")]
     (spit not-histfile "test")))
 
 (defn- clean-histfile! []
-  (util/delete-if-exists (str t-data/test-dir "/utiltest.hist"))
-  (util/delete-if-exists (str t-data/test-dir "/utiltest.bist"))
-  (util/delete-if-exists (str t-data/test-dir "/utiltest.txt"))
+  (util/delete-if-exists (str t-data/test-dir "/dummy.hist"))
+  (util/delete-if-exists (str t-data/test-dir "/dummy.bist"))
+  (util/delete-if-exists (str t-data/test-dir "/dummy.txt"))
   (util/delete-if-exists t-data/test-dir))
 
 (with-state-changes [(before :facts (prepare-histfile!))
                      (after :facts (clean-histfile!))]
   (fact "hist->bist"
+    (util/hist->bist (str t-data/test-dir "/dummy.txt")) => (throws Exception)
     (util/dir-hist->bist t-data/test-dir) => anything
-    (.exists (io/file (str t-data/test-dir "/utiltest.bist"))) => truthy
-    (util/hist->bist (str t-data/test-dir "/utiltest.txt")) => (throws Exception)))
+    (.exists (io/file (str t-data/test-dir "/dummy.bist"))) => truthy))
