4c5d69e0573e77e4f45cc4b1d28059f11f5d774b
atsuo yamada <yamada-git@tir.jp>
2016-02-08 15:53:21 +0900

    Move some vars from t-client to t-data

--- test/shtrom/t_client.clj
+++ test/shtrom/t_client.clj
@@ -1,35 +1,11 @@
 (ns shtrom.t-client
   (:require [midje.sweet :refer :all]
             [ring.adapter.jetty :as jetty]
+            [shtrom.t-data :as t-data]
             [shtrom.config :as config]
             [shtrom.cache :as cache]
             [shtrom.handler :as handler]
             [shtrom.client :as client]))
-
-(def test-key "0")
-(def test-ref "test")
-(def test-bin-size 64)
-(def test-values [345 127 493 312])
-(def test-values-first [(first test-values)])
-
-(def long-test-refs ["test-long-a" "test-long-b" "test-long-c" "test-long-d" "test-long-e" "test-long-f" "test-long-g" "test-long-h"])
-(def max-value 128)
-(def long-test-values (take 100000 (repeatedly #(rand-int max-value))))
-
-(def ^:private bin-sizes [64
-                          128
-                          256
-                          512
-                          1024
-                          2048
-                          4096
-                          8192
-                          16384
-                          32768
-                          65536
-                          131072
-                          262144
-                          524288])
 
 (defn concurrent-reduce
   [key refs bin-size initial-values]
@@ -38,7 +14,7 @@
   (doall
    (pmap
     (fn [r]
-      (doseq [s bin-sizes]
+      (doseq [s t-data/bin-sizes]
         (client/reduce-hist key r s)))
     refs))
   nil)
@@ -66,31 +42,31 @@
                                       (run-server!)))
                      (after :facts (shutdown-server!))]
   (fact "save/load/reduce histogram"
-    (client/save-hist test-key test-ref test-bin-size []) => (throws RuntimeException "Empty values")
-    (client/save-hist test-key test-ref test-bin-size test-values) => nil
-    (client/load-hist "not" "found" test-bin-size 0 256) => [0 0 (list)]
-    (client/load-hist test-key test-ref test-bin-size 0 256) => [0 256 test-values]
-    (client/load-hist test-key test-ref test-bin-size -1 256) => [0 256 test-values]
-    (client/load-hist test-key test-ref test-bin-size 256 0) => [0 0 (list)]
-    (client/load-hist test-key test-ref test-bin-size 1 1) => [0 64 test-values-first]
-    (client/reduce-hist "not" "found" test-bin-size) => (throws RuntimeException #"Invalid key, ref or bin-size")
-    (client/reduce-hist test-key test-ref test-bin-size) => nil
-    (client/delete-hist test-key) => nil))
+    (client/save-hist t-data/client-key t-data/client-ref t-data/client-bin-size []) => (throws RuntimeException "Empty values")
+    (client/save-hist t-data/client-key t-data/client-ref t-data/client-bin-size t-data/client-values) => nil
+    (client/load-hist "not" "found" t-data/client-bin-size 0 256) => [0 0 (list)]
+    (client/load-hist t-data/client-key t-data/client-ref t-data/client-bin-size 0 256) => [0 256 t-data/client-values]
+    (client/load-hist t-data/client-key t-data/client-ref t-data/client-bin-size -1 256) => [0 256 t-data/client-values]
+    (client/load-hist t-data/client-key t-data/client-ref t-data/client-bin-size 256 0) => [0 0 (list)]
+    (client/load-hist t-data/client-key t-data/client-ref t-data/client-bin-size 1 1) => [0 64 t-data/client-values-first]
+    (client/reduce-hist "not" "found" t-data/client-bin-size) => (throws RuntimeException #"Invalid key, ref or bin-size")
+    (client/reduce-hist t-data/client-key t-data/client-ref t-data/client-bin-size) => nil
+    (client/delete-hist t-data/client-key) => nil))
 
 (with-state-changes [(before :facts (do
                                       (client/shtrom-init "test.shtrom-client.config.clj")
                                       (run-server!)))
                      (after :facts (shutdown-server!))]
   (fact "concurrently reduce histogram"
-    (concurrent-reduce test-key long-test-refs test-bin-size long-test-values) => nil
-    (client/delete-hist test-key) => nil))
+    (concurrent-reduce t-data/client-key t-data/client-long-refs t-data/client-bin-size t-data/client-long-values) => nil
+    (client/delete-hist t-data/client-key) => nil))
 
 (fact "config file not found"
   (client/shtrom-init) => (throws java.lang.RuntimeException))
 
 (with-state-changes [(before :facts (client/shtrom-init "test.shtrom-client.config.clj"))]
   (fact "for server error"
-    (client/save-hist test-key test-ref test-bin-size test-values) => nil
-    (client/load-hist test-key test-ref test-bin-size 0 256) => nil
-    (client/reduce-hist test-key test-ref test-bin-size) => nil
-    (client/delete-hist test-key) => nil))
+    (client/save-hist t-data/client-key t-data/client-ref t-data/client-bin-size t-data/client-values) => nil
+    (client/load-hist t-data/client-key t-data/client-ref t-data/client-bin-size 0 256) => nil
+    (client/reduce-hist t-data/client-key t-data/client-ref t-data/client-bin-size) => nil
+    (client/delete-hist t-data/client-key) => nil))
