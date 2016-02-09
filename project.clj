7edf612c0036aa506ceb07fdf6fa27fd6253475a
Takashi AOKI <me@tak.sh>
2016-02-09 00:11:48 -0800

    fix project url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject shtrom "0.1.0-SNAPSHOT"
   :description "Fast data storage for depth and coverage of genome"
-  :url "http://github.com/chrovis/shtrom"
+  :url "https://github.com/chrovis/shtrom"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
