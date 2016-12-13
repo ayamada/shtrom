1ec3fb74de4e32e684961322177f23b6c6164ed3
atsuo yamada <yamada-git@tir.jp>
2016-12-13 15:06:01 +0900

    Fix indent

--- src/java/shtrom/util/IOUtil.java
+++ src/java/shtrom/util/IOUtil.java
@@ -12,130 +12,130 @@ import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 
 public class IOUtil {
-	public static ByteBuffer genByteBuffer (int size) {
-		return ByteBuffer.allocate(size).order(ByteOrder.BIG_ENDIAN);
-	}
+    public static ByteBuffer genByteBuffer (int size) {
+        return ByteBuffer.allocate(size).order(ByteOrder.BIG_ENDIAN);
+    }
 
-	public static byte[] httpBodyToBytes (InputStream input, int len) throws IOException {
-		ByteBuffer bb = genByteBuffer(len);
-		int bufSize = 4096;
-		byte[] buf = new byte[bufSize];
-		while (true) {
-			int l = input.read(buf, 0, bufSize);
-			if (l < 0) { break; }
-			bb.put(buf, 0, l);
-		}
-		return bb.array();
-	}
+    public static byte[] httpBodyToBytes (InputStream input, int len) throws IOException {
+        ByteBuffer bb = genByteBuffer(len);
+        int bufSize = 4096;
+        byte[] buf = new byte[bufSize];
+        while (true) {
+            int l = input.read(buf, 0, bufSize);
+            if (l < 0) { break; }
+            bb.put(buf, 0, l);
+        }
+        return bb.array();
+    }
 
-	public static int[] byteArrayToData (byte[] src, int len) {
-		ByteBuffer bb = genByteBuffer(len);
-		bb.put(src, 0, len);
-		bb.position(0);
-		int resultLength = len / 4;
-		int[] result = new int[resultLength];
-		for (int i = 0; i < resultLength; i++) {
-			result[i] = bb.getInt();
-		}
-		return result;
-	}
+    public static int[] byteArrayToData (byte[] src, int len) {
+        ByteBuffer bb = genByteBuffer(len);
+        bb.put(src, 0, len);
+        bb.position(0);
+        int resultLength = len / 4;
+        int[] result = new int[resultLength];
+        for (int i = 0; i < resultLength; i++) {
+            result[i] = bb.getInt();
+        }
+        return result;
+    }
 
-	private static void readBytes (DataInputStream rdr, byte[] buf, int offset, int l) throws EOFException, IOException {
-		int totalRead = 0;
-		while (totalRead < l) {
-			int n = rdr.read(buf, offset + totalRead, l - totalRead);
-			if (n < 0) { throw new EOFException("Premature EOF"); }
-			totalRead += n;
-		}
-	}
+    private static void readBytes (DataInputStream rdr, byte[] buf, int offset, int l) throws EOFException, IOException {
+        int totalRead = 0;
+        while (totalRead < l) {
+            int n = rdr.read(buf, offset + totalRead, l - totalRead);
+            if (n < 0) { throw new EOFException("Premature EOF"); }
+            totalRead += n;
+        }
+    }
 
-	private static void readByteBuffer (DataInputStream rdr, ByteBuffer bb, int l) throws EOFException, IOException {
-		int cap = bb.capacity();
-		readBytes(rdr, bb.array(), 0, l);
-		bb.limit(cap);
-		bb.position(l);
-	}
+    private static void readByteBuffer (DataInputStream rdr, ByteBuffer bb, int l) throws EOFException, IOException {
+        int cap = bb.capacity();
+        readBytes(rdr, bb.array(), 0, l);
+        bb.limit(cap);
+        bb.position(l);
+    }
 
-	private static int bReadInteger (DataInputStream rdr) throws EOFException, IOException {
-		ByteBuffer bb = genByteBuffer(8);
-		readByteBuffer(rdr, bb, 4);
-		bb.flip();
-		return bb.getInt();
-	}
+    private static int bReadInteger (DataInputStream rdr) throws EOFException, IOException {
+        ByteBuffer bb = genByteBuffer(8);
+        readByteBuffer(rdr, bb, 4);
+        bb.flip();
+        return bb.getInt();
+    }
 
-	public static int[] bistRead (File f) throws IOException {
-		int len = (int)(f.length() / 4);
-		int[] result = new int[len];
-		DataInputStream rdr = new DataInputStream(new FileInputStream(f));
-		try {
-			for (int i = 0; i < len; i++) {
-				result[i] = bReadInteger(rdr);
-			}
-		}
-		finally {
-			rdr.close();
-		}
-		return result;
-	}
+    public static int[] bistRead (File f) throws IOException {
+        int len = (int)(f.length() / 4);
+        int[] result = new int[len];
+        DataInputStream rdr = new DataInputStream(new FileInputStream(f));
+        try {
+            for (int i = 0; i < len; i++) {
+                result[i] = bReadInteger(rdr);
+            }
+        }
+        finally {
+            rdr.close();
+        }
+        return result;
+    }
 
-	public static int[] bistReadWithRange (File f, int left, int right) throws IOException {
-		if (right <= left) { return new int[0]; }
-		int len = right - left;
-		int[] result = new int[len];
-		DataInputStream rdr = new DataInputStream(new FileInputStream(f));
-		try {
-			rdr.skipBytes(left * 4);
-			for (int i = 0; i < len; i++) {
-				result[i] = bReadInteger(rdr);
-			}
-		}
-		finally {
-			rdr.close();
-		}
-		return result;
-	}
+    public static int[] bistReadWithRange (File f, int left, int right) throws IOException {
+        if (right <= left) { return new int[0]; }
+        int len = right - left;
+        int[] result = new int[len];
+        DataInputStream rdr = new DataInputStream(new FileInputStream(f));
+        try {
+            rdr.skipBytes(left * 4);
+            for (int i = 0; i < len; i++) {
+                result[i] = bReadInteger(rdr);
+            }
+        }
+        finally {
+            rdr.close();
+        }
+        return result;
+    }
 
-	public static void bistWrite (File f, int[] values) throws IOException {
-		DataOutputStream wtr = new DataOutputStream(new FileOutputStream(f));
-		int len = values.length;
-		try {
-			for (int i = 0; i < len; i++) {
-				ByteBuffer bb = genByteBuffer(8);
-				bb.putInt(values[i]);
-				wtr.write(bb.array(), 0, 4);
-			}
-		}
-		finally {
-			wtr.close();
-		}
-	}
+    public static void bistWrite (File f, int[] values) throws IOException {
+        DataOutputStream wtr = new DataOutputStream(new FileOutputStream(f));
+        int len = values.length;
+        try {
+            for (int i = 0; i < len; i++) {
+                ByteBuffer bb = genByteBuffer(8);
+                bb.putInt(values[i]);
+                wtr.write(bb.array(), 0, 4);
+            }
+        }
+        finally {
+            wtr.close();
+        }
+    }
 
-	public static int valuesToContentLength (int[] values) {
-		return 16 + (4 * values.length);
-	}
+    public static int valuesToContentLength (int[] values) {
+        return 16 + (4 * values.length);
+    }
 
-	public static byte[] valuesToContent (long start, long end, int[] values) {
-		int len = values.length;
-		int bbLen = valuesToContentLength(values);
-		ByteBuffer bb = genByteBuffer(bbLen);
-		bb.putLong(start);
-		bb.putLong(end);
-		for (int i = 0; i < len; i++) {
-			bb.putInt(values[i]);
-		}
-		return bb.array();
-	}
+    public static byte[] valuesToContent (long start, long end, int[] values) {
+        int len = values.length;
+        int bbLen = valuesToContentLength(values);
+        ByteBuffer bb = genByteBuffer(bbLen);
+        bb.putLong(start);
+        bb.putLong(end);
+        for (int i = 0; i < len; i++) {
+            bb.putInt(values[i]);
+        }
+        return bb.array();
+    }
 
-	public static int[] reduce (int[] values) {
-		int srcSize = values.length;
-		int resultSize = (srcSize + 1) / 2;
-		int [] result = new int[resultSize];
-		for (int i = 0; i < srcSize; i = i + 2) {
-			int v1 = values[i];
-			int i2 = i + 1;
-			int v2 = (i2 < srcSize) ? values[i2] : 0;
-			result[i/2] = v1 + v2;
-		}
-		return result;
-  }
+    public static int[] reduce (int[] values) {
+        int srcSize = values.length;
+        int resultSize = (srcSize + 1) / 2;
+        int [] result = new int[resultSize];
+        for (int i = 0; i < srcSize; i = i + 2) {
+            int v1 = values[i];
+            int i2 = i + 1;
+            int v2 = (i2 < srcSize) ? values[i2] : 0;
+            result[i/2] = v1 + v2;
+        }
+        return result;
+    }
 }
